ec21d89de3c978230a9fb3bf674f6e1e6517e345
20lisa-asil02 <lisa2002kren@yandex.ru>
2022-12-14 17:57:20 +0300

    написала обработчики кнопок, создала отдельный класс для кнопок и основного экрана

--- Pens.java
+++ Pens.java
@@ -9,18 +9,46 @@ import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 
-public class Pens extends Stationery {
-    JLabel label = new JLabel("Picture", new ImageIcon("https://sun9-35.userapi.com/impg/egG_d6sY1rHwpeEj_KNaHU6nwIpQxjbdKP3KKQ/PkXEw_Blyhw.jpg?size=320x320&quality=96&sign=f1cc911db57bd6b96a861ca959c2cf95&type=album"), JLabel.CENTER);
-    JLabel label1 = new JLabel("Метка");
+public class Pens extends JPanel {
 
-    @Override
+    JPanel panel = new JPanel();
+
+    //JLabel label = new JLabel("Picture", new ImageIcon("https://sun9-35.userapi.com/impg/egG_d6sY1rHwpeEj_KNaHU6nwIpQxjbdKP3KKQ/PkXEw_Blyhw.jpg?size=320x320&quality=96&sign=f1cc911db57bd6b96a861ca959c2cf95&type=album"), JLabel.CENTER);
+    //JLabel label1 = new JLabel("Метка");
+
+    public Pens() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        panel.setLayout(new GridLayout(3, 3));
+
+        setBackground(new Color(180, 142, 196));
+        //panel.setLocation(0, 0);
+        //panel.setSize(getWidth(), getHeight());
+        panel.setBackground(new Color(0, 0, 0));
+
+        panel.setLayout(new GridLayout(2, 0, 5, 12));
+
+        for (int i = 0; i < 8; i++) {
+            panel.add(new JButton("Кнопка " + i));
+        }
+
+        add(panel);
+
+        setVisible(true);
+    }
+
+
+}
+
+    /*@Override
     protected void paintStationery(java.awt.Graphics g) {
+
         Graphics2D g1 = (Graphics2D) g;
         try {
             URL url = new URL("https://img.freepik.com/free-photo/boxes-with-gifts-christmas-decorations-blue-surface_127032-2004.jpg?w=996&t=st=1670335922~exp=1670336522~hmac=1e26e0ccb14772b6359335486c396b43536f1dfdb180772b575e00ffe490dd87");
             BufferedImage img = ImageIO.read(url);
             g1.drawImage(img, 1, 1, this);
-            URL url2 = new URL("https://sun9-35.userapi.com/impg/egG_d6sY1rHwpeEj_KNaHU6nwIpQxjbdKP3KKQ/PkXEw_Blyhw.jpg?size=320x320&quality=96&sign=f1cc911db57bd6b96a861ca959c2cf95&type=album");
+            //URL url2 = new URL("https://sun9-35.userapi.com/impg/egG_d6sY1rHwpeEj_KNaHU6nwIpQxjbdKP3KKQ/PkXEw_Blyhw.jpg?size=320x320&quality=96&sign=f1cc911db57bd6b96a861ca959c2cf95&type=album");
+            URL url2 = new URL("https://s3.ibta.ru/goods/143299/172d96ca751fe8b5fd63eed0f822f5cc_x.jpg");
             BufferedImage img2 = ImageIO.read(url2);
             Image scaleImage = img2.getScaledInstance(200, 200,Image.SCALE_DEFAULT);
             g1.drawImage(scaleImage, 100, 100, this);
@@ -38,4 +66,4 @@ public class Pens extends Stationery {
     public String getButtonName() {
         return "Ручки";
     }
-}
+}*/
--- ShopProject.java
+++ ShopProject.java
@@ -2,6 +2,9 @@ package ITProject;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuKeyListener;
+import javax.swing.event.MenuListener;
 import java.awt.event.*;
 
 import java.awt.image.BufferedImage;
@@ -9,53 +12,125 @@ import java.net.URL;
 
 import static java.awt.FlowLayout.*;
 
-public class ShopProject extends JFrame {
-    private final JPanel panel1;
-    private final JPanel panel2;
+public class ShopProject extends JFrame implements MenuListener {
+    //private final JPanel panel1;
+    //private final JPanel panel2 = new JPanel(); // панель для кнопок
+
+    //JPanel panel3;
+
+    JPanel panel = new JPanel();     // общая панель для всего фрейма
+
+    int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+    int width =Toolkit.getDefaultToolkit().getScreenSize().width;
+    Buttons buttons = new Buttons(width, 50, this); // класс для кнопок
+
+    Basic basic = new Basic(width, height - 50, "MainFrame"); // класс для основного экрана
 
     public ShopProject() {
         super("Shop");
-        setLayout(null);
-        this.setBackground(new Color(0x05C3FC));
-        panel1 = new JPanel(new BorderLayout());
+        //setLayout(null);
+
+        /*panel1 = new JPanel();
         panel2 = new JPanel();
-        init();
+        panel3 = new JPanel();
+        double width = getWidth();
+        double height = getHeight();*/
+
+        /*panel1.setSize(getWidth(), 500);
+        panel1.setLocation(0,0);
+        panel2.setSize(100, getHeight() - 500);
+        panel2.setLocation(0, 500);
+        pack();
+        init();*/
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        panel.add(buttons);
+        panel.add(basic);
+
+        getContentPane().add(panel);
+
+        setSize(1500, 800);
+        /*this.setBackground(new Color(0x05C3FC));
+        panel1 = new JPanel(new BorderLayout());
+        panel1.add(new MainFrame());
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.X_AXIS)); /// обавила то что в скобках
+        panel3 = new JPanel();
+        panel3.setSize(100, 100);
+        panel3.setLocation(300, 400);
+        panel1.add(panel3);///
+
+
+        pack();
+        init();*/
+
     }
 
-    private void init() {
+    /*private void init() {
         add(new Button("Start"));
-        panel2.setLayout(null);
-        JPanel panel3 = new JPanel();
-        panel3.setBounds(0, 35, 125, 800);
-        panel3.setBackground(new Color(0xB6D5E5));
-        add(panel3);
-        JPanel panel4 = new JPanel();
-        panel4.setBounds(1155, 35, 125, 800);
-        panel4.setLayout(null);
-        panel4.setBackground(new Color(0xB6D5E5));
-        add(panel3);
-        add(panel4);
         add(panel1);
         add(panel2);
+
+
+
+        //panel2.setLayout(null);
         panel2.setBackground(new Color(0xE5E5F6));
-        panel2.setBounds(0,0,1280,35);
-        panel1.setBounds(125, 35, 1030, 800);
+        panel2.setBounds(0,0,1500,35);
+        panel1.setBounds(125, 35, 1500, 800);
         setSize(1500, 800);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        addButton(new MainFrame(), new Pens(), new Pencil(),
+        /*addButton(new MainFrame(), new Pens(), new Pencil(),
                 new Ruler(), new Backpack(), new Notebooks(),
                 new Paints(), new Paper(), new Glue(),
-                new ForChildren(), new Others(), new Discounts());
-    }
+                new ForChildren(), new Others(), new Discounts());/
 
-    private void addButton(Stationery...k) {
+        //addButton();
+
+        setSize(1500, 800);
+
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }*/
+
+
+    /*private void addButton() {
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(MainFrame);
+        MainFrame.setBorderPainted(false);
+        MainFrame.setContentAreaFilled(false);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Pens);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Pencil);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Ruler);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Backpack);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Notebooks);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Paints);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Paper);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Glue);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(ForChildren);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Others);
+        panel2.add(Box.createHorizontalGlue());
+        panel2.add(Discounts);
+        panel2.add(Box.createHorizontalGlue());
+    }*/
+
+    /*private void addButton(Stationery...k) {
         int l =  0;
         for (Stationery st : k) {
             JButton button = new JButton(st.getButtonName());
             button.setSize(90, 35);
-            button.setLocation(l * 105 + 10, 0);
+            button.setLocation(l * 100 + 10, 0);
             button.setBorderPainted(false);
             button.setContentAreaFilled(false);
             button.setFocusPainted(false);
@@ -63,8 +138,108 @@ public class ShopProject extends JFrame {
             button.addActionListener(new Action(st));
             l += 1;
         }
+    }*/
+
+    @Override
+    public void menuSelected(MenuEvent e) {   //обработчик для кнопок
+        if (e.getSource() == buttons.Pens)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Pens");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Pencil)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Pencil");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Discounts)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Discounts");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Others)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Others");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Glue)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Glue");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.ForChildren)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "ForChildren");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Ruler)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Ruler");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.MainFrame)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "MainFrame");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Paper)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Paper");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Backpack)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Backpack");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Notebooks)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Notebooks");
+            panel.add(basic);
+            panel.updateUI();
+        }
+        if (e.getSource() == buttons.Paints)
+        {
+            panel.remove(basic);
+            basic = new Basic(width, height - 50, "Paints");
+            panel.add(basic);
+            panel.updateUI();
+        }
+
     }
-    private class Action implements ActionListener {
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+
+    /*private class Action implements ActionListener {
         Stationery mc;
         private Action(Stationery st)
         {
@@ -81,7 +256,7 @@ public class ShopProject extends JFrame {
         panel1.add(f);
         panel1.revalidate();
         panel1.repaint();
-    }
+    }*/
 }
 class ButtonTest {
     public static void main(String[] args) {
@@ -95,6 +270,5 @@ class ButtonTest {
         });
     }
 }
-
 
 
