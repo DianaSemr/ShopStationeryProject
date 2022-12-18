36b6fa0ba643eb20f658e4a1cabad3641ddfa221
Diana <105105971+DianaSemr@users.noreply.github.com>
2022-12-18 18:08:35 +0300

    акции

---
+++ BasicDisc.java
@@ -0,0 +1,161 @@
+package ITProject;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.plaf.DimensionUIResource;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+public class BasicDisc extends JPanel implements ActionListener {
+    JButton button = new JButton("подробнее");
+    JPanel all_panel = new JPanel();
+    public BasicDisc(int width, int height, String url, String str, String text) {
+
+
+        all_panel.setLayout(new CardLayout(0, 0)); ///
+
+        all_panel.setPreferredSize(new Dimension(650, 345));//350 345
+        all_panel.setBackground(new Color(253, 251, 251));
+        //JPanel Panel = new JPanel(); ///
+        setPreferredSize(new Dimension(width, height));  ///
+        setBackground(new Color(253, 251, 251));  //
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); ////
+
+        all_panel.add(new BasicDisc.Picture(width, height, url, str, text));
+        all_panel.add(new BasicDisc.Text(text));
+        add(all_panel);
+
+
+
+        /*JPanel panel_for_picture = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panel_for_picture.setOpaque(false);
+        panel_for_picture.setPreferredSize(new Dimension(350, 350));
+
+        try {
+            URL u = new URL(url);
+            BufferedImage img = ImageIO.read(u);
+            Image scaleImage = img.getScaledInstance(340, 340,Image.SCALE_DEFAULT);
+            JLabel picture = new JLabel(new ImageIcon(scaleImage));
+            panel_for_picture.add(picture);
+            Panel.add(panel_for_picture); ////
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+
+        JPanel panel_for_label = new JPanel(new FlowLayout(FlowLayout.CENTER));    // панелька для цены
+        panel_for_label.setOpaque(false);
+        /*JLabel label = new JLabel();       // лейбл цена
+        label.setText("Цена: "+str+" ₽  ");
+        Font newFont = new Font("Serif", Font.PLAIN, 25);
+        label.setFont(newFont);
+        panel_for_label.add(label);*/
+
+
+
+        Font font = new Font("Serif", Font.PLAIN, 25);
+        //JButton button = new JButton("подробнее");
+        button.setFont(font);
+        button.setBorderPainted(false);//убираем очертания кнопки
+        button.setContentAreaFilled(false);//убираем заливку кнопки
+        //button.setFocusPainted(false);//убираем фокус на кнопке
+        panel_for_label.add(button);
+
+        //Buttons buttons = new Buttons(url, text);
+        //Stationery st = buttons;
+        //button.addActionListener(new Action(st));//добавляем действие
+        button.addActionListener(this);
+
+        add(panel_for_label);
+
+        //Panel.add(buttons);  ////
+
+        //add(Panel);
+
+        setVisible(true);
+
+    }
+
+    class Picture extends JPanel
+    {
+        Picture(int width, int height, String url, String str, String text)
+        {
+            setBackground(new Color(253, 251, 251));
+            JPanel panel_for_picture = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            panel_for_picture.setOpaque(false);
+            panel_for_picture.setPreferredSize(new Dimension(350, 350));
+
+            try {
+                URL u = new URL(url);
+                BufferedImage img = ImageIO.read(u);
+                Image scaleImage = img.getScaledInstance(340, 340,Image.SCALE_DEFAULT);
+                JLabel picture = new JLabel(new ImageIcon(scaleImage));
+                panel_for_picture.add(picture);
+                add(panel_for_picture); ////
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    }
+
+    class Text extends JPanel{
+        Text(String text){
+            setBackground(new Color(253, 251, 251));
+
+            JPanel panel_for_text = new JPanel();
+            panel_for_text.setBackground(new Color(253, 251, 251));
+            panel_for_text.setPreferredSize(new Dimension(350, 350));
+
+            JLabel label_text = new JLabel();
+            label_text.setPreferredSize(new Dimension(350, 350));
+            label_text.setHorizontalAlignment(JLabel.LEFT);
+            label_text.setText(text);
+            Font newFont = new Font("Serif", Font.PLAIN, 20);
+            label_text.setFont(newFont);
+
+            panel_for_text.add(label_text);
+
+            add(panel_for_text);
+        }
+
+    }
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            ((CardLayout) all_panel.getLayout()).next(all_panel);
+        }
+    }
+
+
+
+    /*private class Action implements ActionListener {
+        Stationery mc;
+        private Action(Stationery st)
+        {
+            mc = st;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            paint(mc);
+        }
+    }
+    private void paint(Stationery f) {
+        removeAll();//panel1
+        setLayout(new BorderLayout());
+        add(f);
+        revalidate();
+        repaint();
+    }*/
+
+
+}
--- Discounts.java
+++ Discounts.java
@@ -1,11 +1,80 @@
 package ITProject;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Rectangle2D;
 public class Discounts extends Stationery {
+    public Discounts() {
+
+        JPanel all = new JPanel();   // общая панель
+        all.setLayout(new BoxLayout(all, BoxLayout.Y_AXIS));
+        setBackground(new Color(171, 236, 243));
+        setPreferredSize(new Dimension(getWidth(), 2000));
+
+        Animation animation = new Animation(0, 0, "https://semyaivera.ru/wp-content/uploads/2016/01/Girlyanda-zhivaya.gif");
+        all.add(animation);
+
+        JPanel panel_for_name = new JPanel(new FlowLayout(FlowLayout.CENTER));    // панелька для названя
+        JLabel name = new JLabel();       // лейбл название
+        name.setText("Ручки");
+        Font newFont = new Font("Serif", Font.ITALIC, 70);
+        name.setFont(newFont);
+
+        panel_for_name.add(name);          // добавляем лейбл на панель с названием
+        //panel_for_name.setBackground(new Color(29, 131, 246));
+
+        name.setOpaque(false);
+        panel_for_name.setOpaque(false);
+        all.add(panel_for_name);        // добавляем панель с названием в общую панель
+
+
+        JPanel panel_for_end = new JPanel();
+        panel_for_end.setPreferredSize(new Dimension(50, 60));
+        //panel_for_end.setSize(500, 1000);
+        //panel_for_end.setBackground(new Color(0,0,0));
+        panel_for_end.setOpaque(false);
+        all.add(panel_for_end);
+
+
+        JPanel panel = new JPanel();       //  общая панель для картинок
+        //panel.setSize(getWidth(), getHeight());
+
+        GridLayout grid = new GridLayout(6, 1, 50, 50);    //  панелька-табличка
+        panel.setLayout(grid);
+        BasicDisc panel1 = new BasicDisc(650, 400, "https://s3.ibta.ru/goods/143300/c2da6b873cd8ca8a27e3359076dc8b83_xl.jpg", "27", "<html>"+"<p>Характеристики</p>"+ "<p>Цвет чернил — синий.</p>"+"<p>Стиль — для офиса и учебы.</p>"+"<p>Длина сменного стержня — 140 мм.</p>"+"<p>Толщина линии письма — 0.35 мм.</p>"+"<p>Эргономичная зона захвата — рифление.</p>"+"</html>");
+        BasicDisc panel2 = new BasicDisc(650, 400, "https://s3.ibta.ru/goods/140057/82f81d9873f12ab4f1203a4e745989b3_l.jpg", "15", "<html>"+"<p>Характеристики</p>"+ "<p>Цвет чернил — красный.</p>"+"<p>Серия — Ultra Orange.</p>"+"</html>");
+        BasicDisc panel3 = new BasicDisc(650, 400, "https://s3.ibta.ru/goods/143227/7a7077ea65b238f4b7670c01475b5084_l.jpg", "30", "<html>"+"<p>Характеристики</p>"+ "<p>Цвет чернил — красный.</p>"+"<p>Серия — Ultra Orange.</p>"+"</html>");
+        BasicDisc panel4 = new BasicDisc(650, 400, "https://s3.ibta.ru/goods/143562/57c62c06436dd03ca0e12cf59279bde6_l.jpg", "17", "<html>"+"<p>Характеристики</p>"+ "<p>Цвет чернил — красный.</p>"+"<p>Серия — Ultra Orange.</p>"+"</html>");
+        BasicDisc panel5 = new BasicDisc(650, 400, "https://s3.ibta.ru/goods/143444/c4963ab41f85604e9d22eb59c1d907ed_l.jpg", "49", "<html>"+"<p>Характеристики</p>"+ "<p>Цвет чернил — красный.</p>"+"<p>Серия — Ultra Orange.</p>"+"</html>");
+        BasicDisc panel6 = new BasicDisc(650, 400, "https://s3.ibta.ru/goods/140053/b2b00dd3f6af3827c2aaba0789a8c189_l.jpg", "24","<html>"+"<p>Характеристики</p>"+ "<p>Цвет чернил — красный.</p>"+"<p>Серия — Ultra Orange.</p>"+"</html>");
+
+
+        panel.add(panel1);
+        panel.add(panel2);
+        panel.add(panel3);
+        panel.add(panel4);
+        panel.add(panel5);
+        panel.add(panel6);
+
+        /*for (int i = 0; i < 6; i++) {
+            JPanel picture = new JPanel();    //  одна клеточка
+            picture.setPreferredSize(new Dimension(400, 400));
+            picture.setBackground(new Color(243, 236, 236));
+            panel.add(picture);    // добавляем клеточку на панель для картинок
+
+        }*/
+
+        panel.setOpaque(false);
+        all.add(panel);
+
+        all.setOpaque(false);
+        add(all);
+
+        setVisible(true);
+    }
     @Override
     protected void paintStationery(java.awt.Graphics g) {
         Graphics2D g1 = (Graphics2D) g;
